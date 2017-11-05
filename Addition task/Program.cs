b457fe62e57a9fbe0676c688f2c19a8484aaf88b
maxxwell131 <maxxwell131@yahoo.com>
2017-11-05 20:31:47 +0200

    Добавьте файлы проекта.

---
+++ Addition task/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition_task
+{
+    /*
+     * Задание
+        *Используя Visual Studio, создайте проект по шаблону Console Application.
+        *Требуется:
+        *Создать класс с именем Address.
+        *В теле класса требуется создать поля: index, country, city, street, house, apartment. Для каждого
+        *поля, создать свойство с двумя методами доступа.
+        *Создать экземпляр класса Address.
+        *В поля экземпляра записать информацию о почтовом адресе.
+        *Выведите на экран значения полей, описывающих адрес.
+*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
